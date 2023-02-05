481781d14b943a6b63320131fa04438ef5ed968c
Alexuber <alexuberbuber@gmail.com>
2023-02-05 14:16:13 +0200

    add js for fixed header, clean code, W3C validator

--- js/hero-scroll.js
+++ js/hero-scroll.js
@@ -1,26 +1,17 @@
-const headerSectionEl = document.querySelector('.header');
+const headerSectionEl = document.querySelector('[data-header]');
 const aboutSectionEl = document.querySelector('.about');
-const priceSecEl = document.querySelector('.price');
-const benefitsSectionEl = document.querySelector('.benefits');
-const teamSectionEl = document.querySelector('.team');
-const gallerySectionEl = document.querySelector('.gallery');
 
 window.onscroll = () => changeHeaderBackground();
 
 function changeHeaderBackground() {
   const headerOffsetTrigger = headerSectionEl.offsetHeight;
-  const aboutOffsetTrigger = aboutSectionEl.offsetHeight;
-  const priceOffsetTrigger = priceSecEl.offsetHeight;
-  const benefitsOffsetTrigger = benefitsSectionEl.offsetHeight;
-  const teamOffsetTrigger = teamSectionEl.offsetHeight;
+  const aboutOffsetTrigger = aboutSectionEl.offsetTop;
 
   const pageOffset = window.pageYOffset;
 
-  console.log('🆑  pageOffset', pageOffset);
-
-  console.log('🆑  aboutSectionEl.offsetHeight', aboutSectionEl.offsetHeight);
-
-  if (pageOffset > aboutOffsetTrigger) {
-    headerSectionEl.classList.add('transparency');
+  if (pageOffset + headerOffsetTrigger > aboutOffsetTrigger) {
+    headerSectionEl.classList.add('header-scroll-dark');
+  } else {
+    headerSectionEl.classList.remove('header-scroll-dark');
   }
 }
